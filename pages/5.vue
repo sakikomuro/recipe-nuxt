b1c3efe47646b2cfb1ddd6f28170b657ac2c219e
saki <sa.0920.ko@gmail.com>
2023-04-08 15:28:15 +0900

    鶏むね肉の甘辛唐揚げレシピ記載

--- pages/5.vue
+++ pages/5.vue
@@ -4,7 +4,7 @@ import HeaderComponent from '../components/HeaderComponent.vue';
   <div class="pb-10">
     <HeaderComponent></HeaderComponent>
     <div class="w-4/5 lg:w-3/5 mx-auto">
-      <h1 class="pt-10 font-bold text-2xl">鶏胸肉の甘辛唐揚げ</h1>
+      <h1 class="pt-10 font-bold text-2xl">鶏むね肉の甘辛唐揚げ</h1>
 
       <div class="flex flex-col space-y-9 lg:flex-row lg:items-center py-10">
         <div class="flex flex-col flex-1">
@@ -16,40 +16,43 @@ import HeaderComponent from '../components/HeaderComponent.vue';
             class="flex flex-col space-y-1 [&>li]:border-dotted [&>li]:border-slate-300 [&>li]:border-b-2 [&>li]:flex [&>li]:justify-between lg:px-5"
           >
             <li>
-              <p>エリンギ</p>
-              <p>1パック</p>
+              <p>鶏むね肉</p>
+              <p>250g</p>
             </li>
             <li>
-              <p>まいたけ</p>
-              <p>1パック</p>
-            </li>
-            <li>
-              <p>ブナピー</p>
-              <p>1パック</p>
-            </li>
-
-            <p class="text-sm">（好きなきのこで結構です）</p>
-
-            <h3>A</h3>
-            <li>
-              <p>白だし</p>
+              <p>薄力粉</p>
               <p>大さじ3</p>
             </li>
             <li>
-              <p>レモン汁</p>
-              <p>大さじ2</p>
-            </li>
-            <li>
-              <p>お酢</p>
-              <p>大さじ2</p>
-            </li>
-            <li>
-              <p>オリーブオイル</p>
+              <p>片栗粉</p>
               <p>大さじ1</p>
             </li>
+
             <li>
-              <p>こしょう</p>
-              <p>少々</p>
+              <p>白いりごま</p>
+              <p>適量</p>
+            </li>
+            <li>
+              <p>油</p>
+              <p>適量</p>
+            </li>
+
+            <h3>A</h3>
+            <li>
+              <p>しょうゆ</p>
+              <p>大さじ2</p>
+            </li>
+            <li>
+              <p>みりん</p>
+              <p>大さじ2</p>
+            </li>
+            <li>
+              <p>砂糖</p>
+              <p>大さじ3</p>
+            </li>
+            <li>
+              <p>料理酒</p>
+              <p>大さじ2</p>
             </li>
           </ul>
         </div>
@@ -58,18 +61,23 @@ import HeaderComponent from '../components/HeaderComponent.vue';
       <div>
         <h2 class="font-bold py-5">&lt; 作り方 &gt;</h2>
         <ul class="list-decimal px-5 flex flex-col space-y-1">
+          <li>鶏肉を一口大に切り、薄力粉をまぶす。</li>
           <li>
-            きのこをフライパンに入れ、強火で熱する。この時きのこをあまり動かさない。
-          </li>
-          <li>きのこが汗をかいてきたら上下返し、軽く炒める。</li>
-          <li>
-            一旦きのこを皿に取り出し、同じフライパンにAを入れ軽く煮立たせる。
+            鍋に油を注ぎ、<span class="font-bold">1</span
+            >を揚げる。鶏肉に火が通り、きつね色になったら油を切る。
           </li>
           <li>
-            取り出しておいたきのこを<span class="font-bold">3</span
-            >に入れ、混ぜ合わせる
+            別のフライパンに<span class="font-bold">A</span
+            >を入れ、中火で煮立たせる。
           </li>
-          <li>完成</li>
+          <li>
+            水溶き片栗粉を<span class="font-bold">3</span
+            >に少しずつ混ぜ合わせ、とろみをつける。
+          </li>
+          <li>
+            <span class="font-bold">4</span>に<span class="font-bold">2</span
+            >を入れ、鶏肉全体にソースを絡ませたら完成。
+          </li>
         </ul>
       </div>
     </div>
